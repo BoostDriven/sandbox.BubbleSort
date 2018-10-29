d7c5932c18a7a0dcb88c57717e72ad7e9cdc15de
tptaylor <troytaylor.net@gmail.com>
2018-10-29 11:51:43 -0400

    Add project files.

---
+++ BubbleSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class BubbleSort
+    {
+        //static void Main(string[] args)
+        //{
+        //    int[] number = { 89, 76, 45, 92, 12, 99 };
+        //    bool flag = true;
+        //    int temp;
+        //    int numLength = number.Length;
+
+        //    Console.WriteLine("Original array :");
+        //    foreach (int aa in number)
+        //        Console.WriteLine(aa + " ");
+
+        //    for (int i = 1; (i <= (numLength - 1)) && flag; i++)
+        //    {
+        //        flag = false;
+        //        for (int j = 0; j < (numLength - 1); j++)
+        //        {
+        //            if (number[j + 1] > number[j])
+        //            {
+        //                temp = number[j];
+        //                number[j] = number[j + 1];
+        //                number[j + 1] = temp;
+        //                flag = true;
+        //            }
+        //        }
+        //    }
+
+            //Console.WriteLine("\n" + "Sorted array :");
+            //foreach (int num in number)
+            //{
+            //    Console.WriteLine($"\t {num}");
+            //}
+
+            //Console.ReadLine();
+        //}
+    }
+}
---
+++ BubbleSortv2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BubbleSortv2
+{
+    class BubbleSortv2
+    {
+        public static void Main()
+        {
+            int[] a = { 3, 0, 2, 5, -1, 4, 1 };
+            int t;
+            Console.WriteLine("Original array :");
+
+            foreach (int aa in a)
+                Console.Write(aa + " ");
+            Console.Write("\n");
+
+            for (int p = 0; p <= a.Length - 2; p++)
+            {
+                for (int i = 0; i <= a.Length - 2; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        t = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = t;
+                    }
+                }
+            }
+
+            Console.WriteLine("\n" + "Sorted array :");
+            foreach (int aa in a)
+                Console.Write(aa + " ");
+            Console.Write("\n");
+            Console.ReadLine();
+        }
+    }
+}
